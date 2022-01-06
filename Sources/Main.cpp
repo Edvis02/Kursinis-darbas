b3c07dc0d90a1208336f72bc4e9f034654c09a6d
Edvinas Butkus <e.butkus@stud.vilniustech.lt>
2022-01-06 18:31:05 +0200

    Ball bouncing around Ball bounces of paddles Bounces random direction

--- Sources/Main.cpp
+++ Sources/Main.cpp
@@ -1,5 +1,4 @@
 #include <SFML/Graphics.hpp>
-#include <Cmath>
 
 using namespace sf;
 using namespace std;
@@ -15,7 +14,7 @@ const int BALL_HEIGHT = 12;
 
 struct Ball
 {
-	float x, y;
+	float x, y, dx, dy;
 };
 struct Paddle1
 {
@@ -46,14 +45,25 @@ void UpdatePositionPaddle2(Paddle2& paddle2)
 		if (paddle2.y > 0)
 			paddle2.y -= dy;
 }
-void UpdatePositionBall(Ball& ball, float& dy)
+void UpdatePositionBall(Ball& ball, Paddle1& paddle1, Paddle2& paddle2)
 {
-	const float dx = 10.0f;
-	ball.x += dx;
-	if (ball.y <= 0 || ball.y >= WINDOW_HEIGHT)
-		ball.dy *= -1;
-	if (ball.x < paddle1.x && (abs(ball.y - paddle1.y) > PADDLE_HEIGHT / 2));
+	ball.x += ball.dx;
+	ball.y += ball.dy;
 	
+	if (ball.x > paddle2.x && paddle2.y - BALL_HEIGHT < ball.y && ball.y < paddle2.y + PADDLE_HEIGHT)
+	{
+		ball.dx = -ball.dx;
+		ball.dy += rand() % 5 - 3;
+	}
+	if (ball.x < paddle1.x && paddle1.y - BALL_HEIGHT < ball.y && ball.y < paddle1.y + PADDLE_HEIGHT)
+	{
+		ball.dx = -ball.dx;
+		ball.dy += rand() % 5 - 3;
+	}
+	if (ball.y <= 0)
+		ball.dy = fabs(ball.dy);
+	if (ball.y >= WINDOW_HEIGHT)
+		ball.dy = -fabs(ball.dy);
 }
 
 int main()
@@ -76,6 +86,8 @@ int main()
 	Ball ball;
 	ball.x = 394;
 	ball.y = 250;
+	ball.dx = 10;
+	ball.dy = 0;
 
 	Paddle1 paddle1;
 	paddle1.x = 0;
@@ -84,8 +96,6 @@ int main()
 	Paddle2 paddle2;
 	paddle2.x = WINDOW_WIDTH - 12;
 	paddle2.y = WINDOW_HEIGHT / 2;
-
-	float dy = 0.5;
 
 	while (app.isOpen())
 	{
@@ -98,7 +108,7 @@ int main()
 
 		UpdatePositionPaddle1(paddle1);
 		UpdatePositionPaddle2(paddle2);
-		UpdatePositionBall(ball, dy);
+		UpdatePositionBall(ball, paddle1 ,paddle2);
 
 		app.draw(sprBackground);
 
