56977d52001bb3a7ca99badbdd0375b76597c1bf
Edvinas Butkus <e.butkus@stud.vilniustech.lt>
2022-05-09 09:51:13 +0300

    Fixed score board

---
+++ Sources/Utils.h
@@ -0,0 +1,60 @@
+#pragma once
+
+void UpdatePositionPaddle1(Paddle1& paddle1)
+{
+	const float dy = 10.0f;
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+		if (paddle1.y < WINDOW_HEIGHT - PADDLE_HEIGHT)
+			paddle1.y += dy;
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+		if (paddle1.y > 0)
+			paddle1.y -= dy;
+}
+void UpdatePositionPaddle2(Paddle2& paddle2)
+{
+	const float dy = 10.0f;
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+		if (paddle2.y < WINDOW_HEIGHT - PADDLE_HEIGHT)
+			paddle2.y += dy;
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+		if (paddle2.y > 0)
+			paddle2.y -= dy;
+}
+void UpdatePositionBall(Ball& ball, Paddle1& paddle1, Paddle2& paddle2, float& score1, float& score2)
+{
+	ball.x += ball.dx;
+	ball.y += ball.dy;
+
+	if (ball.x > paddle2.x && paddle2.y - BALL_HEIGHT < ball.y && ball.y < paddle2.y + PADDLE_HEIGHT)
+	{
+		ball.dx = -ball.dx;
+		ball.dy += rand() % 5 - 3;
+	}
+	if (ball.x < paddle1.x && paddle1.y - BALL_HEIGHT < ball.y && ball.y < paddle1.y + PADDLE_HEIGHT)
+	{
+		ball.dx = -ball.dx;
+		ball.dy += rand() % 5 - 3;
+	}
+	if (ball.y <= 0)
+	{
+		ball.dy = fabs(ball.dy);
+	}
+	if (ball.y >= WINDOW_HEIGHT)
+		ball.dy = -fabs(ball.dy);
+	if (ball.x > WINDOW_WIDTH)
+	{
+		ball.dy = 0;
+		ball.dx = -ball.dx;
+		ball.x = 394;
+		ball.y = 250;
+		score1 += 1;
+	}
+	if (ball.x < -BALL_WIDTH)
+	{
+		ball.dy = 0;
+		ball.dx = -ball.dx;
+		ball.x = 394;
+		ball.y = 250;
+		score2 += 1;
+	}
+}
